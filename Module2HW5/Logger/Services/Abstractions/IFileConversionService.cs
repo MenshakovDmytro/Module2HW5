3df28d295e3899db56e696ba8be9a785466f18fe
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:36:55 +0200

    added IFileConversionService

---
+++ Module2HW5/Logger/Services/Abstractions/IFileConversionService.cs
@@ -0,0 +1,9 @@
+﻿using Logger.Models;
+
+namespace Logger.Services.Abstractions
+{
+    public interface IFileConversionService
+    {
+        public Config ConvertJsonToConfig(string jsonConfig);
+    }
+}
