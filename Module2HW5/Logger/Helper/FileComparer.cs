24b4542f2fba92aee63c4b79b6eab831b4332017
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:35:17 +0200

    added FileComparer

---
+++ Module2HW5/Logger/Helper/FileComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace Logger.Helper
+{
+    public class FileComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            var firstItem = x as string;
+            var secondItem = y as string;
+            return File.GetCreationTimeUtc(firstItem).CompareTo(File.GetCreationTimeUtc(secondItem));
+        }
+    }
+}
