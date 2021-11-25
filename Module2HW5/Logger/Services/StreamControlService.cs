fb28d1e7b683431fbc034efd5b2f9eb2239448dc
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:38:53 +0200

    added StreamControlService

---
+++ Module2HW5/Logger/Services/StreamControlService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Logger.Services.Abstractions;
+
+namespace Logger.Services
+{
+    public class StreamControlService : IStreamControlService, IDisposable
+    {
+        private readonly StreamWriter _streamWriter;
+
+        public StreamControlService(string path)
+        {
+            _streamWriter = new StreamWriter(path, true) { AutoFlush = true };
+        }
+
+        ~StreamControlService()
+        {
+            Dispose();
+        }
+
+        public void WriteData(string data)
+        {
+            _streamWriter.WriteLine(data);
+        }
+
+        public void Dispose()
+        {
+            _streamWriter.Close();
+            _streamWriter.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
