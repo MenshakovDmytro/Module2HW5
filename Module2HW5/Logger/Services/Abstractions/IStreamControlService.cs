7e2e82d270f248a5965ec0da5780f523c098fbe6
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:37:31 +0200

    added IStreamControlService

---
+++ Module2HW5/Logger/Services/Abstractions/IStreamControlService.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using Logger.Models;
+
+namespace Logger.Services.Abstractions
+{
+    public interface IStreamControlService
+    {
+        public void WriteData(string data);
+    }
+}
