abd30cdd2caabd6c49b452a2cc85bfa1e0b45df7
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:34:59 +0200

    added Models

---
+++ Module2HW5/Logger/Models/LoggerConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Logger.Models
+{
+    public class LoggerConfig
+    {
+        public string TimeFormat { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string DirectoryPath { get; set; }
+    }
+}
