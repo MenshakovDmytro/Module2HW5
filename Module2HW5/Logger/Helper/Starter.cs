c4946f4ac7ba289f0f3b51d29102a602e32f388d
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:35:41 +0200

    added Starter

---
+++ Module2HW5/Logger/Helper/Starter.cs
@@ -0,0 +1,29 @@
+﻿using Logger.Helper;
+using Logger.Providers;
+using Logger.Providers.Abstractions;
+using Logger.Services;
+using Logger.Services.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logger
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var start = new ServiceCollection()
+                .AddTransient<IFileService, FileService>()
+                .AddTransient<IStreamControlService, StreamControlService>()
+                .AddSingleton<ILoggerService, LoggerService>()
+                .AddSingleton<IConfigService, ConfigService>()
+                .AddTransient<IConfigProvider, ConfigProvider>()
+                .AddSingleton<IFileConversionService, JsonConversionService>()
+                .AddTransient<IActionsService, ActionsService>()
+                .AddTransient<App>()
+                .BuildServiceProvider();
+            var app = start.GetService<App>();
+            app.StartWork();
+            start.Dispose();
+        }
+    }
+}
