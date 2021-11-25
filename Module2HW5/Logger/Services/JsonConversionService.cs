767c9154fe103364314c7008120c415a49e29fe7
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:38:29 +0200

    added JsonConversionService

---
+++ Module2HW5/Logger/Services/JsonConversionService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Logger.Models;
+using Logger.Services.Abstractions;
+using Newtonsoft.Json;
+
+namespace Logger.Services
+{
+    public class JsonConversionService : IFileConversionService
+    {
+        public Config ConvertJsonToConfig(string jsonConfig)
+        {
+            var configFile = File.ReadAllText(jsonConfig);
+            var config = JsonConvert.DeserializeObject<Config>(configFile);
+            return config;
+        }
+    }
+}
