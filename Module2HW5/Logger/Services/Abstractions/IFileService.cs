73b1f2a1e1b51418d2dccf2abff561f9e3fd3164
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:37:06 +0200

    added IFileService

---
+++ Module2HW5/Logger/Services/Abstractions/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace Logger.Services.Abstractions
+{
+    public interface IFileService
+    {
+        public void WriteToFile(string data);
+        public void CreateDirectory(string directoryName);
+        public void CreateFile(string fileName);
+        public void RemoveExcessFiles(string path);
+    }
+}
