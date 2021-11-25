40ac11998d0cb2d805b085106102484bff0a13ec
MenshakovDmytro <jgadf1kz@gmail.com>
2021-11-25 20:37:17 +0200

    added ILoggerService

---
+++ Module2HW5/Logger/Services/Abstractions/ILoggerService.cs
@@ -0,0 +1,13 @@
+﻿using Logger.Models;
+
+namespace Logger.Services.Abstractions
+{
+    public interface ILoggerService
+    {
+        public void LogError(string message);
+        public void LogInfo(string message);
+        public void LogWarning(string message);
+        public void Log(LogType logType, string log);
+        public void WriteLogToFile(string message);
+    }
+}
